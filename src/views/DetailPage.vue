bbfe2523a8ab98caad14cd70172d7209589c9726
Ricardo Tavares <rnunot@gmail.com>
2020-08-31 11:50:33 +0100

    Replicate question's code

---
+++ src/views/DetailPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <my-component v-if="showComponent" />
+    <div @click="showComponent = true">Show Component</div>
+  </div>
+</template>
+
+<script>
+const MyComponent = () => import(/* webpackChunkName: "MyComponent-chunk" */ "@/components/HelloWorld.vue");
+export default {
+  name: "DetailPage",
+  components: {
+    MyComponent,
+  },
+  data() {
+    return {
+      showComponent: false
+    }
+  }
+};
+</script>
---
+++ src/views/MasterPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="navigate">
+    Some text
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MasterPage",
+  methods: {
+    navigate() {
+      this.$router.push({
+        path: "/DetailPage",
+        query: {},
+      });
+    },
+  },
+};
+</script>
